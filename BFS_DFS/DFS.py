5325f5242b8c0de742358f25e7228570eea4477f
yubin <yubin@qq.com>
2021-04-27 17:33:35 +0800

    Add fixed DFS

--- BFS_DFS/DFS.py
+++ BFS_DFS/DFS.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 gragh = {"A": ["B", "C"],
          "B": ["A", "C", "D"],
          "C": ["A", "B", "D", "E"],
@@ -24,8 +25,12 @@ def DFS(gragh, root):
 
         print(key)
 
+# 当以A为root，走ABDECF时，上述的DFS会输出：ABDEFC
+# 原因是 上述的dfs 是标记邻节点的方法，C在B遍历完后已经被压入栈了，所以C会后打印。每pop一次才会print。
+# 改正的这个dfs，标记的是弹出节点，只有弹出了，才算放到set里，但这样会导致栈中有大量重复节点。print是放在if下面，只有不在set里的才print。
+# 只有极个别例子会出现这个问题。
 
-def DFS_fix(gragh,root):
+def DFS_fix(gragh, root):
 
     stack = []
     stack.append(root)
